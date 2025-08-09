3924d003c9756dc625eb0857d96d4d6166a665e2
imvaibhav99 <imvaibhav9796@gmail.com>
2025-08-09 20:25:01 +0530

    changes in constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,9 @@
-const BASE_URL = "http://localhost:7777";
 
 
 //For production,use this "/api" else localhost:7777
-// const BASE_URL = "/api";
+
+const BASE_URL = import.meta.env.PROD
+  ? "/api"
+  : "http://localhost:7777";
+
 export default BASE_URL;
