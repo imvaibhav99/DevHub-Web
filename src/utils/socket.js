128138981572e5a24547ea8be1c7c4dca0119ed6
imvaibhav99 <imvaibhav9796@gmail.com>
2025-08-14 20:47:36 +0530

    socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,10 +2,10 @@ import { io } from "socket.io-client";
 import BASE_URL from "./constants";
 
 export const createSocketConnection=()=>{
-    if(location.hostname === "localhost"){      
-        return io(BASE_URL);      //connecting to the server
+    if(location.hostname === 'localhost' ){
+         return io(BASE_URL); 
+    }else{
+        return io("/",{path: "/api/socket.io"})
     }
-    else{
-        return io("/",{ path: "/api/socket.io" });     //connecting to the server in production environment with the path /api/socket.io
-    }
+        //connecting to the server
 } 
